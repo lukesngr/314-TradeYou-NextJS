4f3c5606d64f357be598b447190f6501ce7dbb88
Luke Sanger <lukesngr@gmail.com>
2023-05-14 16:43:50 +1000

    Got logic for nonmembers done just gotta do members

--- pages/api/completeJob.js
+++ pages/api/completeJob.js
@@ -15,16 +15,64 @@ export default async(req, res) => {
                 }
             });
 
-            result += await prisma.payment.create({
-                data: {
-                    amount: 100.0,
-                    ServiceRequest: {
-                        connect: {id: data.serviceRequestID}
+            const ids = await prisma.serviceRequest.findUnique({
+                where: {
+                    id: data.serviceRequestID
+                },
+                select: {
+                    TradeYouProfessional: {
+                        select: {id: true}
+                    },
+                    TradeYouUser: {
+                        select: {id: true}
+                    }
+                }
+            })
+
+            const userMembershipCategory = await prisma.tradeYouUser.findUnique({
+                where: {
+                    id: ids.TradeYouUser.id
+                },
+                select: {
+                    MembershipPlan: {
+                        select: {category: true}
+                    }
+                }
+            })
+
+            const profMembershipCategory = await prisma.tradeYouProfessional.findUnique({
+                where: {
+                    id: ids.TradeYouProfessional.id
+                },
+                select: {
+                    MembershipPlan: {
+                        select: {category: true}
                     }
                 }
             })
 
 
+            if(userMembershipCategory.category != "membership") {
+                result += await prisma.payment.create({
+                    data: {
+                        amount: 100.0,
+                        dateTime: new Date(),
+                        ServiceRequest: {
+                            connect: {id: data.serviceRequestID}
+                        }
+                    }
+                })
+            }
+
+            if(profMembershipCategory != "membership") {
+                result += await prisma.charges.create({
+                    data: {
+                        amount: 20.0,
+                        dateTime: new Date(),
+                        TradeYouProfessional: { connect: {id: ids.TradeYouProfessional.id}}
+                    }
+                })
+            }
 
             res.status(200).json(result);
         }
